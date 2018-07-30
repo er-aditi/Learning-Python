be92dd5949d81a9cd9fa1a1111c5702de8b25c9d
er-aditi <aditijainnoida85@gmail.com>
2018-07-30 19:24:35 +0530

    Looping_EOD

---
+++ List Working Programs/List_Simple Statistics.py
@@ -0,0 +1,9 @@
+
+data = list(range(1, 11))
+print(data)
+
+min_value = min(data)
+print(min_value)
+
+print(max(data))
+print(sum(data))
---
+++ List Working Programs/List_exponents_range.py
@@ -0,0 +1,12 @@
+squares = []
+for value in range(1, 11):
+
+    # square = value**2
+
+    squares.append(value**2)
+
+print(squares)
+
+for val in squares:
+    print(val)
+
