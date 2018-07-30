e81e9d214898d9dde63a802039d04b112b98030a
er-aditi <aditijainnoida85@gmail.com>
2018-07-30 15:59:30 +0530

    Hello

--- List Working Programs/List_Looping1..py
+++ List Working Programs/List_Looping1..py
@@ -6,8 +6,10 @@ for car in cars:
     print("Car: " + car)
     for city in cities:
         print("City: " + city)
+        print("stores")
 
 print("Hello")
+print("Vineet")
 
 
 # for car in cars:
