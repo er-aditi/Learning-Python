fc8ef677a9c9858227685e5299ae0e6405d8abfd
er-aditi <aditijainnoida85@gmail.com>
2018-07-30 18:55:30 +0530

    Looping_EOD

---
+++ List Working Programs/List_Loop_even-odd number.py
@@ -0,0 +1,13 @@
+even_numbers = list(range(2, 21, 2))
+print(even_numbers)
+
+for even_number in even_numbers:
+    print("These are the even values: ".title() + str(even_number))
+
+print("\n")
+
+odd_numbers = list(range(1, 20, 2))
+print(odd_numbers)
+
+for odd_number in odd_numbers:
+    print("These are the odd Values: ".title().strip() + str(odd_number))
---
+++ List Working Programs/List_skip_range.py
@@ -0,0 +1,6 @@
+even_numbers = list(range(2, 21, 2))
+print(even_numbers)
+
+for even_num in even_numbers:
+    print("These are the Even Numbers: " + str(even_num))
+
