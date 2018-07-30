04f3e066e9d028694c4d39a91995d163d1f55b98
er-aditi <aditijainnoida85@gmail.com>
2018-07-30 18:16:43 +0530

    Looping

---
+++ List Working Programs/List_Indenting Unnecessarily.py
@@ -0,0 +1,6 @@
+magicians = ['david', 'aleen', 'tom']
+for magician in magicians:
+    print("I Like to see magic: " + magician)
+    print("I am very Grateful to see:  " + magician + "\n")
+
+    print("It is Best: " + magician)
---
+++ List Working Programs/List_Pizzas.py
@@ -0,0 +1,11 @@
+Pizzas = ['onion', 'capsicum', 'corn']
+for pizza in Pizzas:
+    print("I Like " + pizza.title() + " pizza")
+    print("I like pepperoni pizza.".title(), "\n")
+
+print("I Love it: " + pizza)
+
+print("I really love pizza!")
+
+
+print(pizza)
