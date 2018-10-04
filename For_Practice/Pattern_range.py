91f0a58dbc463dc9c4eef9c30f8ce2eb147c47da
er-aditi <aditijainnoida85@gmail.com>
2018-10-04 18:54:04 +0530

    for

---
+++ For_Practice/Pattern_range.py
@@ -0,0 +1,7 @@
+for num in range(1, 6):
+
+    for num1 in range(5, 0, -1):
+        if num >= num1:
+            print(num1, end="")
+
+    print(" ")
---
+++ For_Practice/pattern.py
@@ -0,0 +1,7 @@
+for limit in range(1, 6):
+
+    for num in range(5, 0, -1):
+        if limit <= num:
+            print(num, end=" ")
+
+    print(" ")
