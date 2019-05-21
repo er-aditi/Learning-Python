9777ed003e239a367b96824485ec4fc7d29eb77e
Aditi <aditijainnoida85@gmail.com>
2019-05-21 17:55:58 +0530

    Assert

---
+++ Selenium Scripts/Assert.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+import unittest
+
+
+class AssertTitle(unittest.TestCase):
+
+    def setUp(self):
+        global driver
+        driver = webdriver.Chrome("C:\\Users\\Aditi\\Python_3\\chromedriver.exe")
+        # driver.implicitly_wait(10)
+        driver.get("https://travelingtony.weebly.com")
+
+    def test_AssertTitle(self):
+        self.assertEqual(driver.title, "Traveling Tony's Photography - Welcome")
+
+    def tearDown(self):
+        driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+print("Very Good")
